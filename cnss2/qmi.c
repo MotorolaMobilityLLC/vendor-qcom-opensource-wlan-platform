1e5e0e8ca498f8dadf22478b00ed4afce084dd8c
Haichao Li <lihc3@lenovo.com>
2025-09-26 11:59:15 +0800

    sm8635/wcn7851:eiger:add dedicated bdf for eiger
    Eiger evt production:
    add dedicated bdf for eiger
    
    Change-Id: I61fa250292b103044b0534d2ccf0ddddba054641
    Signed-off-by: Haichao Li <lihc3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3572201
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Qiuhuan Zhao <zhaoqh3@lenovo.com>
    Tested-by: Jira Key
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- cnss2/qmi.c
+++ cnss2/qmi.c
@@ -118,6 +118,7 @@ static moto_product products_list[] = {
 	{"arcfox",      "ROW",  "row.epa"},
 	{"arcfox",      "JPN",  "jpn.epa"},
 	{"oberon",	"all",	NV_EPA}, //a19110 IKSWT-74269
+	{"eiger",	"all",	NV_EPA}, //IKSWW-53636
 	/* Terminator */
 	{{0}, {0}, {0}},
 };
